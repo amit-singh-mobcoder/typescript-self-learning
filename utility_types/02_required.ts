d12def7a84355b7cc93b699fb9c8d21212b43cce
amit-singh-mobcoder <amit.s@mobcoder.com>
2024-12-27 12:38:00 +0530

    remove ts error like already declared

--- utility_types/02_required.ts
+++ utility_types/02_required.ts
@@ -19,3 +19,6 @@ console.log('user1==>',user1)
 /*
 partial.ts and required.ts declare a type or interface with the same name (USER), and when they are in the same folder and part of the same project, the TypeScript compiler considers them part of the same scope. As a result, it treats the USER type or interface as being declared twice, which is not allowed.
 */
+
+// or simple we can do this at the end the of any file
+export {}
